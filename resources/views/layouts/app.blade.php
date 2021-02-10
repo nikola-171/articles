3821f7eb3b8ffe84af1df2424b81b7caf15a33f1
nikola <nikola.tosic98@outlook.com>
2021-02-10 09:55:11 +0100

    ispravljena greska, logout nije hteo da se prikaze

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,14 +17,9 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('style')
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
 </head>
 <body>
@@ -88,8 +83,6 @@
         <main class="py-4">
             @yield('content')
         </main>
-
-        @yield('script')
     </div>
 </body>
 </html>
